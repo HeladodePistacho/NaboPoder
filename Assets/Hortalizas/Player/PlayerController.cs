b474003b123c6761e6792489b3d94825511a944e
HeladodePistacho <th_sola@hotmail.com>
2020-04-15 20:15:10 +0200

    Player Movement done

--- Assets/Hortalizas/Player/PlayerController.cs
+++ Assets/Hortalizas/Player/PlayerController.cs
@@ -9,14 +9,15 @@ public class PlayerController : MonoBehaviour
     public float movement_acceleration = 0.0f;
     public float friction = 0.0f;
 
-    float current_movement_speed = 0.0f;
+    Vector3 current_movement_speed;
 
+    int last_axis = 0;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        current_movement_speed = new Vector3(0.0f, 0.0f, 0.0f);
     }
 
     // Update is called once per frame
@@ -27,32 +28,40 @@ public class PlayerController : MonoBehaviour
 
     void HandleMovement()
     {
-        HorizontalMovement();
+        int h_axis = (int)Input.GetAxisRaw("Horizontal");
+        int v_axis = (int)Input.GetAxisRaw("Vertical");
 
+        if (Mathf.Sign(current_movement_speed.x) != h_axis)
+            current_movement_speed.x += (float)h_axis * friction * Time.deltaTime;
 
+        if (Mathf.Sign(current_movement_speed.y) != v_axis)
+            current_movement_speed.y += (float)v_axis * friction * Time.deltaTime;
 
+        current_movement_speed.x += (float)h_axis * movement_acceleration * Time.deltaTime;
+        current_movement_speed.y += (float)v_axis * movement_acceleration * Time.deltaTime;
 
-    }
+        current_movement_speed.x = Mathf.Clamp(current_movement_speed.x, -max_move_speed, max_move_speed);
+        current_movement_speed.y = Mathf.Clamp(current_movement_speed.y, -max_move_speed, max_move_speed);
 
-    void HorizontalMovement()
-    {
-
-        float h_axis = Input.GetAxisRaw("Horizontal");
-        float v_axis = Input.GetAxisRaw("Vertical");
-
-        current_movement_speed += h_axis * movement_acceleration * Time.deltaTime;
-
-        current_movement_speed = Mathf.Clamp(current_movement_speed, -max_move_speed, max_move_speed);
-
-        if(h_axis == 0)
+        if (h_axis == 0)
         {
             //Decelerate
-            current_movement_speed -= Mathf.Sign(current_movement_speed) * friction * Time.deltaTime;
-            if (Mathf.Abs(current_movement_speed) < 0.5) current_movement_speed = 0.0f;
+            current_movement_speed.x -= Mathf.Sign(current_movement_speed.x) * friction * Time.deltaTime;
+            if (Mathf.Abs(current_movement_speed.x) < 0.5) current_movement_speed.x = 0.0f;
+        }
+
+        if (v_axis == 0)
+        {
+            //Decelerate
+            current_movement_speed.y -= Mathf.Sign(current_movement_speed.y) * friction * Time.deltaTime;
+            if (Mathf.Abs(current_movement_speed.y) < 0.5) current_movement_speed.y = 0.0f;
         }
 
 
-        transform.Translate(current_movement_speed * Time.deltaTime, 0.0f, 0.0f);
+        transform.Translate(current_movement_speed * Time.deltaTime);
+
+
+
     }
    
 }
