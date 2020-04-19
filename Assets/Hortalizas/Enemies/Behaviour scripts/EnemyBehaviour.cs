1da365d316a0113851a84f134d8b98845d5841a9
fran_botttos <fran.rgimenez@gmail.com>
2020-04-19 19:06:11 +0200

    Enemy moving with A*

--- Assets/Hortalizas/Enemies/Behaviour scripts/EnemyBehaviour.cs
+++ Assets/Hortalizas/Enemies/Behaviour scripts/EnemyBehaviour.cs
@@ -6,24 +6,20 @@ using Pathfinding;
 public enum EnemyState
 {
     IDLE,
-    GOING_TO,
+    BACK_TO_INIT_POS,
     FOLLOWING_PLAYER,
-    WAITING_FOR_PLAYER
+    HIT_PLAYER
 }
 
 public class EnemyBehaviour : MonoBehaviour
 {
-    public LayerMask layerForWaitingForPlayer;
-
-    //public float maxDistanceToPlayer = 10f;
     private EnemyStats enemyStats;
+    private Vector3 initPosition;
 
     public EnemyState enemyState = EnemyState.IDLE;
-
     public HortalizaPathfinding pathfinding;
 
     Camera main;
-    //PossibleTargets
     Transform player;
 
     //Animations
@@ -32,11 +28,10 @@ public class EnemyBehaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //TODO: search for player
+        initPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
         main = Camera.main;
         player = GameObject.FindGameObjectWithTag("Player").transform;
         enemyStats = GetComponent<EnemyStats>();
-        SetState_FollowPlayer();
     }
 
     // Update is called once per frame
@@ -44,99 +39,78 @@ public class EnemyBehaviour : MonoBehaviour
     {
         switch (enemyState)
         {
-            case EnemyState.WAITING_FOR_PLAYER:
-                Update_SeekingForPlayer();
-                break;
             case EnemyState.FOLLOWING_PLAYER:
                 Update_FollowingPlayer();
                 break;
+            case EnemyState.BACK_TO_INIT_POS:
+                Update_BackToInitialPos();
+                break;
+            case EnemyState.IDLE:
+                Update_Idle();
+                break;
+            case EnemyState.HIT_PLAYER:
+                Update_HitPlayer();
+                break;
         }
     }
-
     public void SetState_FollowPlayer()
     {
         enemyState = EnemyState.FOLLOWING_PLAYER;
-        pathfinding.SetTarget(player, OnPlayerReached);
+        pathfinding.SetTarget(player, SetState_Attack);
         anim.SetBool("Moving", true);
-        gameObject.layer = LayerMask.NameToLayer("Turnips_FollowingPlayer");
     }
-
-    Vector3 previousPosition = Vector3.zero;
-    float timer = 0f;
-    void Update_FollowingPlayer()
+    public void SetState_BackToInitPos()
     {
-        /* if(Vector3.SqrMagnitude(previousPosition - transform.position) < 1)
-         {
-             timer += Time.deltaTime;
-         }
-         else
-         {
-             previousPosition = transform.position;
-             timer = 0;
-         }
-
-         if(timer > 0.5)
-         {
-             pathfinding.Stop();
-             timer = 0;
-             Debug.Log("Stop!");
-         }*/
-    }
-
-    void OnPlayerReached()
-    {
-        // Debug.Log("reachedPlayer");
-        enemyState = EnemyState.WAITING_FOR_PLAYER;
-        anim.SetBool("Moving", false);
-    }
-
-    void Update_SeekingForPlayer()
-    {
-        if (Vector3.Distance(transform.position, player.position) > enemyStats.sightRange)
-        {
-            Vector2 direction = player.position - transform.position;                                                                                                                                                                        
-
-            RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, direction.normalized, 0.3f, layerForWaitingForPlayer);
-            if (hits.Length <= 1)
-                SetState_FollowPlayer();
-        }
-    }
-
-    public void SetTargetPosition(Vector3 _targetPos)
-    {
-        enemyState = EnemyState.GOING_TO;
-        pathfinding.SetTarget(_targetPos, OnTargetPositionReached);
-
+        enemyState = EnemyState.BACK_TO_INIT_POS;
+        pathfinding.SetTarget(initPosition, SetState_Idle);
         anim.SetBool("Moving", true);
-        gameObject.layer = LayerMask.NameToLayer("Turnips_GoingTo");
-
     }
-
-    void OnTargetPositionReached()
+    public void SetState_Idle()
     {
         enemyState = EnemyState.IDLE;
         anim.SetBool("Moving", false);
-        gameObject.layer = LayerMask.NameToLayer("Turnips_Idle");
     }
-
-    private void OnDrawGizmos()
+    public void SetState_Attack()
     {
-        Gizmos.color = Color.grey;
-        Gizmos.DrawWireSphere(transform.position, enemyStats.sightRange);
-        Gizmos.color = Color.white;
+        enemyState = EnemyState.HIT_PLAYER;
+        anim.SetBool("Moving", false);
+    }
+    void Update_FollowingPlayer()
+    {
+        if (Vector3.Distance(transform.position, player.position) > enemyStats.sightRange)
+        {
+            // Lose sight of the player and go back to init pos
+            SetState_BackToInitPos();
+        }
+    }
+    void Update_BackToInitialPos()
+    {
+        if (Vector3.Distance(transform.position, player.position) < enemyStats.sightRange)
+        {
+            SetState_FollowPlayer();
+        }
+    }
+    void Update_Idle()
+    {
+        if (Vector3.Distance(transform.position, player.position) > enemyStats.sightRange)
+        {
+            SetState_FollowPlayer();
+        }
+    }
+    void Update_HitPlayer()
+    {
+        // TODO: Hit player
+        if (Vector3.Distance(transform.position, player.position) < enemyStats.sightRange)
+        {
+            SetState_FollowPlayer();
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (enemyState == EnemyState.FOLLOWING_PLAYER && collision.collider.CompareTag("Turnip"))
+        if (enemyState == EnemyState.FOLLOWING_PLAYER && collision.collider.CompareTag("Player"))
         {
-            TurnipBehaviour tb = collision.gameObject.GetComponent<TurnipBehaviour>();
-
-            if (tb.turnipState == TurnipState.WAITING_FOR_PLAYER)
-            {
-                // Debug.Log("forceStop");
-                pathfinding.Stop();
-            }
+            // Hit player
         }
     }
 }
