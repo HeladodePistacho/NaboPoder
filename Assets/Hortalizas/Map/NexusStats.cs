079bdc429373c4745bbadbbf09ae9668e803da3c
Carlos Cabreira Sánchez <carcasanchez@users.noreply.github.com>
2020-04-22 19:32:26 +0200

    Fixed some bugs

--- Assets/Hortalizas/Map/NexusStats.cs
+++ Assets/Hortalizas/Map/NexusStats.cs
@@ -1,19 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NexusStats : MonoBehaviour
 {
     public int hp = 20;
+    int initialHP;
+    public Image lifebarUI;
     private SceneHandler sceneHandler;
 
     private void Start()
     {
+        initialHP = hp;
         sceneHandler = GameObject.FindGameObjectWithTag("SceneHandler").GetComponent<SceneHandler>();
     }
     void Update()
     {
-        if(hp <= 0)
+        lifebarUI.fillAmount = (1 / (float)initialHP) * (float)hp;
+
+        if (hp <= 0)
         {
             Destroy(gameObject);
             sceneHandler.GoToNexusDeadScene();
