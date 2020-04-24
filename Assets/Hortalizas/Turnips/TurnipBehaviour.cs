c25ce5ac324b570c038e197bd544601341cc15bf
Carlos Cabreira Sánchez <carcasanchez@users.noreply.github.com>
2020-04-24 16:07:03 +0200

    Fixed negative turnip count bug

--- Assets/Hortalizas/Turnips/TurnipBehaviour.cs
+++ Assets/Hortalizas/Turnips/TurnipBehaviour.cs
@@ -121,6 +121,8 @@ public class TurnipBehaviour : MonoBehaviour
         {
             Destroy(gameObject);
             GameObject p = Instantiate(deadParticles, gameObject.transform.position, gameObject.transform.rotation);
+            GetComponent<Collider2D>().enabled = false;
+            Debug.Log("Nabo destroyed");
             GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStats>().TakeNabos(-1);
             Destroy(p, 3);
             Instantiate(seed, gameObject.transform.position, gameObject.transform.rotation);
